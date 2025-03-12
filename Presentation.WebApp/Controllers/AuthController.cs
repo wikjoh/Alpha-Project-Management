2062b88ca4e558884f7398b357e433e9a00e3af1
wikai <80284758+wikai@users.noreply.github.com>
2025-03-12 14:51:05 +0100

    Setup basic main layout

---
+++ Presentation.WebApp/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.WebApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        [Route("Auth")]
+        public IActionResult Projects()
+        {
+            return View();
+        }
+    }
+}
---
+++ Presentation.WebApp/Controllers/ProjectsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.WebApp.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        //[Route("Projects")]
+        public IActionResult Projects()
+        {
+            return View();
+        }
+    }
+}
