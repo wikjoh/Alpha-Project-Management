56594c9439afb1cfe60447ee49c4e9df4d9e74ca
wikai <80284758+wikai@users.noreply.github.com>
2025-03-16 21:37:10 +0100

    Add project button and refactor layout styles
    Updated `Projects.cshtml` to include a new button for adding projects, utilizing a partial view. Introduced a `ButtonModel` class for button properties. Refactored CSS by removing old styles and adding new layout styles in `_MainLayout.cshtml.css`, `Projects.cshtml.css`, and `_Sidebar.cshtml.css`. Created `_Button.cshtml` and its associated files for button rendering and styling. Added a new SVG icon for the button.

---
+++ Presentation.WebApp/Models/ButtonModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Presentation.WebApp.Models;
+
+public class ButtonModel
+{
+    public string? IconUri { get; set; }
+    public string Title { get; set; } = null!;
+}
---
+++ Presentation.WebApp/Views/Shared/Partials/Components/_Button.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Presentation.WebApp.Views.Shared.Partials.Components
+{
+    public class _ButtonModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
