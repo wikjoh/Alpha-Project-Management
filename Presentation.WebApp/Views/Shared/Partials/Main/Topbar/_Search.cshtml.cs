759777962f7103df975ca6aaef436e4b8bc2ef11
wikai <80284758+wikai@users.noreply.github.com>
2025-03-16 20:35:21 +0100

    Add topbar features and css

---
+++ Presentation.WebApp/Views/Shared/Partials/Main/Topbar/_Search.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Presentation.WebApp.Views.Shared.Partials.Main.Topbar
+{
+    public class _SearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Presentation.WebApp/Views/Shared/Partials/Main/Topbar/_Settings.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Presentation.WebApp.Views.Shared.Partials.Main.Topbar
+{
+    public class _SettingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
