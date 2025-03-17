851a0e32ef780382ad1ffc446a078c5fe8e46bfd
wikai <80284758+wikai@users.noreply.github.com>
2025-03-17 21:54:33 +0100

    Update structure, separate global components into Components-folder, section specific components into Sections-folder.

---
+++ Presentation.WebApp/Views/Shared/Partials/Components/_Logo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Presentation.WebApp.Views.Shared.Partials.Components
+{
+    public class _LogoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
