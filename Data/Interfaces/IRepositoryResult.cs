a6d9d527e8ae9ff967dd3bc1191c3663aed6d097
wikai <80284758+wikai@users.noreply.github.com>
2025-03-30 13:59:32 +0200

    Add repository result interfaces and implementations
    Introduced `IRepositoryResult` and `IRepositoryResult<T>` interfaces to standardize response structures. Added `RepositoryResult` class with static methods for common response scenarios (Ok, BadRequest, NotFound, AlreadyExists, InternalServerError). Implemented a generic `RepositoryResult<T>` class for handling results with a specific type.

---
+++ Data/Interfaces/IRepositoryResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Data.Interfaces;
+
+public interface IRepositoryResult
+{
+    bool Success { get; set; }
+    int StatusCode { get; set; }
+    string? ErrorMessage { get; set; }
+}
+
+public interface IRepositoryResult<T>
+{
+    bool Success { get; set; }
+    int StatusCode { get; set; }
+    T Result { get; set; }
+}
---
+++ Data/Models/RepositoryResult.cs
@@ -0,0 +1,87 @@
+﻿using Data.Interfaces;
+
+namespace Data.Models;
+
+
+public class RepositoryResult : IRepositoryResult
+{
+    public bool Success { get; set; }
+    public int StatusCode { get; set; }
+    public string? ErrorMessage { get; set; }
+
+
+    public static RepositoryResult Ok()
+    {
+        return new RepositoryResult
+        {
+            Success = true,
+            StatusCode = 200
+        };
+    }
+
+    public static RepositoryResult BadRequest(string errorMessage)
+    {
+        return new RepositoryResult
+        {
+            Success = false,
+            StatusCode = 400,
+            ErrorMessage = errorMessage
+        };
+    }
+
+    public static RepositoryResult NotFound(string errorMessage)
+    {
+        return new RepositoryResult
+        {
+            Success = false,
+            StatusCode = 404,
+            ErrorMessage = errorMessage
+        };
+    }
+
+    public static RepositoryResult AlreadyExists(string errorMessage)
+    {
+        return new RepositoryResult
+        {
+            Success = false,
+            StatusCode = 409,
+            ErrorMessage = errorMessage
+        };
+    }
+
+    public static RepositoryResult InternalServerErrror(string errorMessage)
+    {
+        return new RepositoryResult
+        {
+            Success = false,
+            StatusCode = 500,
+            ErrorMessage = errorMessage
+        };
+    }
+}
+
+
+public class RepositoryResult<T> : RepositoryResult, IRepositoryResult<T>
+{
+    public required T Result { get; set; }
+
+    public static RepositoryResult<T> Ok(T result)
+    {
+        return new RepositoryResult<T>
+        {
+            Success = true,
+            StatusCode = 200,
+            Result = result
+        };
+    }
+
+    public static RepositoryResult<T> Created(T result)
+    {
+        return new RepositoryResult<T>
+        {
+            Success = true,
+            StatusCode = 201,
+            Result = result
+        };
+    }
+}
