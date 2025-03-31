a3aab47d574c2aa99575cbb6e5fa822e849d8da8
wikai <80284758+wikai@users.noreply.github.com>
2025-03-31 12:17:33 +0200

    Refactor UserService to include user retrieval logic
    - Added necessary using directives for dependencies.
    - Modified UserService constructor to accept IUserRepository and UserManager<UserEntity>.
    - Introduced GetUsersAsync method to fetch users and map results to UserResult.

--- Business/Services/UserService.cs
+++ Business/Services/UserService.cs
@@ -1,5 +1,22 @@
-﻿namespace Business.Services;
+﻿using Business.Models;
+using Data.Entities;
+using Data.Interfaces;
+using Domain.Extensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
-public class UserService
+namespace Business.Services;
+
+public class UserService(IUserRepository userRepository, UserManager<UserEntity> userManager)
 {
+    private readonly IUserRepository _userRepository = userRepository;
+    private readonly UserManager<UserEntity> _userManager = userManager;
+
+    public async Task<UserResult> GetUsersAsync()
+    {
+        var users = await _userManager.Users.ToListAsync();
+
+        var result = await _userRepository.GetAllAsync();
+        return result.MapTo<UserResult>();
+    }
 }
