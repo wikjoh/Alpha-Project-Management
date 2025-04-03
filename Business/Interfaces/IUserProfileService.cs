14f68ba3ca52d6e7768a2cf39dc74ff59b3d63f7
wikai <80284758+wikai@users.noreply.github.com>
2025-04-03 23:28:54 +0200

    Add IUserProfileService interface and implementation
    Created IUserProfileService interface with methods for managing user profiles. Updated UserProfileService to implement this interface, including methods for creating, retrieving, updating, and deleting user profiles. Enhanced error handling and response mapping for improved functionality.

---
+++ Business/Interfaces/IUserProfileService.cs
@@ -0,0 +1,13 @@
+﻿using Business.Models;
+using Domain.Dtos;
+using Domain.Models;
+
+namespace Business.Interfaces;
+public interface IUserProfileService
+{
+    Task<UserProfileResult<UserProfileModel>> CreateUserProfileAsync(AddUserProfileForm form, string userId);
+    Task<UserProfileResult<UserProfileModel>> DeleteUserProfileAsync(string id);
+    Task<UserProfileResult<IEnumerable<UserProfileModel>>> GetAllUserProfilesAsync();
+    Task<UserProfileResult<UserProfileModel>> GetUserProfileByIdAsync(string id);
+    Task<UserProfileResult<UserProfileModel>> UpdateUserProfileAsync(EditUserProfileForm form);
+}
