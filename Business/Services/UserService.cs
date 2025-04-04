75f717c95eeaa03a0508122391da95ab92db686d
wikai <80284758+wikai@users.noreply.github.com>
2025-04-04 10:31:10 +0200

    Refactor UserService to add user creation functionality
    Updated the UserService class to include SignInManager as a dependency. Removed the GetUsersAsync method and implemented CreateAsync to handle user registration. The new method validates input, checks for existing users, creates a new UserEntity, and manages the user creation process with appropriate success and error responses.

--- Business/Services/UserService.cs
+++ Business/Services/UserService.cs
@@ -1,22 +1,44 @@
 ﻿using Business.Models;
 using Data.Entities;
 using Data.Interfaces;
-using Domain.Extensions;
+using Domain.Models;
 using Microsoft.AspNetCore.Identity;
+using Domain.Dtos;
 using Microsoft.EntityFrameworkCore;
+using Domain.Extensions;
 
 namespace Business.Services;
 
-public class UserService(IUserRepository userRepository, UserManager<UserEntity> userManager)
+public class UserService(IUserRepository userRepository, UserManager<UserEntity> userManager, SignInManager<UserEntity> signInManager)
 {
     private readonly IUserRepository _userRepository = userRepository;
     private readonly UserManager<UserEntity> _userManager = userManager;
+    private readonly SignInManager<UserEntity> _signInManager = signInManager;
 
-    public async Task<UserResult> GetUsersAsync()
+
+    public async Task<UserResult<UserModel>> CreateAsync(UserSignUpForm form)
     {
-        var users = await _userManager.Users.ToListAsync();
+        if (form == null)
+            return UserResult<UserModel>.BadRequest("Form cannot be null.");
 
-        var result = await _userRepository.GetAllAsync();
-        return result.MapTo<UserResult>();
+        if (await _userManager.Users.AnyAsync(u => u.Email == form.Email))
+            return UserResult<UserModel>.AlreadyExists("User with given email address already exists.");
+
+        var userEntity = new UserEntity
+        {
+            UserName = form.Email,
+            Email = form.Email,
+        };
+
+        var result = await _userManager.CreateAsync(userEntity, form.Password);
+        if (result.Succeeded)
+        {
+            var createdUser = (await _userManager.FindByIdAsync(userEntity.Id))!.MapTo<UserModel>();
+            return UserResult<UserModel>.Created(createdUser);
+        }
+        else
+        {
+            return UserResult<UserModel>.InternalServerErrror("Failed creating user.");
+        }
     }
 }
