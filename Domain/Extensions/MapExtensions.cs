b23df019160ac0d6b8ad75cbde17b3f2117d99f4
wikai <80284758+wikai@users.noreply.github.com>
2025-04-19 16:45:57 +0200

    Update MapTo to handle mapping of nested/included objects

--- Domain/Extensions/MapExtensions.cs
+++ Domain/Extensions/MapExtensions.cs
@@ -4,25 +4,60 @@ namespace Domain.Extensions;
 
 public static class MapExtensions
 {
-    public static TDestination MapTo<TDestination>(this object source)
+    public static TDestination MapTo<TDestination>(this object source, HashSet<object>? visited = null)
     {
         ArgumentNullException.ThrowIfNull(source, nameof(source));
 
-        TDestination destination = Activator.CreateInstance<TDestination>()!;
+        // Initialize the visited set if it's null
+        visited ??= new HashSet<object>();
 
-        var sourceProperties = source.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
-        var destinationProperties = destination.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
-
-        foreach (var destinationProperty in destinationProperties)
+        // Check if the source object has already been visited
+        if (visited.Contains(source))
         {
-            var sourceProperty = sourceProperties.FirstOrDefault(x => x.Name == destinationProperty.Name && x.PropertyType == destinationProperty.PropertyType);
-            if (sourceProperty != null && destinationProperty.CanWrite)
+            return default!;
+        }
+
+        // Mark the current object as visited
+        visited.Add(source);
+
+        TDestination destination = Activator.CreateInstance<TDestination>()!;
+        var sourceType = source.GetType();
+        var sourceProps = sourceType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        var destProps = destination.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (var destProp in destProps)
+        {
+            var sourceProp = sourceProps.FirstOrDefault(p => p.Name == destProp.Name && p.CanRead);
+            if (sourceProp == null || !destProp.CanWrite) continue;
+
+            var sourceValue = sourceProp.GetValue(source);
+            if (sourceValue == null)
             {
-                var value = sourceProperty.GetValue(source);
-                destinationProperty.SetValue(destination, value);
+                destProp.SetValue(destination, null);
+                continue;
+            }
+
+            if (destProp.PropertyType == sourceProp.PropertyType)
+            {
+                destProp.SetValue(destination, sourceValue);
+            }
+            else if (IsComplexType(destProp.PropertyType) && IsComplexType(sourceProp.PropertyType))
+            {
+                var mapMethod = typeof(MapExtensions)
+                    .GetMethod(nameof(MapTo), BindingFlags.Public | BindingFlags.Static)!
+                    .MakeGenericMethod(destProp.PropertyType);
+
+                // Pass the visited set to the recursive call
+                var nestedMapped = mapMethod.Invoke(null, new object[] { sourceValue, visited });
+                destProp.SetValue(destination, nestedMapped);
             }
         }
 
         return destination;
+    }
+
+    private static bool IsComplexType(Type type)
+    {
+        return type.IsClass && type != typeof(string);
     }
 }
