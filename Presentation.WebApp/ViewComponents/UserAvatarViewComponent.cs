ea82a60b4b69736fdb0384ec102e153ffc61fc84
wikai <80284758+wikai@users.noreply.github.com>
2025-05-03 21:29:04 +0200

    Fix issue with logged in user avatar not loading under Clients page

--- Presentation.WebApp/ViewComponents/UserAvatarViewComponent.cs
+++ Presentation.WebApp/ViewComponents/UserAvatarViewComponent.cs
@@ -1,17 +1,23 @@
-﻿using Data.Entities;
+﻿using Business.Interfaces;
+using Data.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.WebApp.ViewComponents;
 
-public class UserAvatarViewComponent(UserManager<UserEntity> userManager) : ViewComponent
+public class UserAvatarViewComponent(UserManager<UserEntity> userManager, IMemberService memberService) : ViewComponent
 {
     private readonly UserManager<UserEntity> _userManager = userManager;
+    private readonly IMemberService _memberService = memberService;
 
     public async Task<IViewComponentResult> InvokeAsync()
     {
-        var user = await _userManager.GetUserAsync(HttpContext.User);
-        var imageURI = Url.Content($"~{user?.MemberProfile?.ImageURI}") ?? Url.Content("~/images/memberDefaultAvatar.svg");
+        var result = await _memberService.GetMemberByIdAsync(_userManager.GetUserId(HttpContext.User)!);
+        var memberProfile = result.Data;
+
+        var imageURI = !string.IsNullOrEmpty(memberProfile!.ImageURI)
+            ? Url.Content($"~{memberProfile.ImageURI}")
+            : Url.Content("~/images/memberDefaultAvatar.svg");
 
         return View("Default", imageURI);
     }
