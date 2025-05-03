af8df0549eb3e01aae76b33e125a516a4becf64b
wikai <80284758+wikai@users.noreply.github.com>
2025-05-03 12:47:39 +0200

    Implement view component for retrieving user avatar and update partials to use it. Set name dynamically in Settings partial.

---
+++ Presentation.WebApp/ViewComponents/UserAvatarViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.WebApp.ViewComponents;
+
+public class UserAvatarViewComponent(UserManager<UserEntity> userManager) : ViewComponent
+{
+    private readonly UserManager<UserEntity> _userManager = userManager;
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var user = await _userManager.GetUserAsync(HttpContext.User);
+        var imageURI = Url.Content($"~{user?.MemberProfile?.ImageURI}") ?? Url.Content("~/images/memberDefaultAvatar.svg");
+
+        return View("Default", imageURI);
+    }
+}
